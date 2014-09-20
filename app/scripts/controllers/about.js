e41115f02246421782907feb42cb7146fa0895fb
Steven Cheong <stevenc49@gmail.com>
2014-09-20 00:13:44 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name epermitjsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the epermitjsApp
+ */
+angular.module('epermitjsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name epermitjsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the epermitjsApp
+ */
+angular.module('epermitjsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
