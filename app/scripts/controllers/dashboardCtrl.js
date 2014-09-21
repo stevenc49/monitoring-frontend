441cb7a81bc9efa012df8060afedbd9b5f68db0c
Steven Cheong <stevenc49@gmail.com>
2014-09-20 21:13:32 -0700

    application data schema

--- app/scripts/controllers/dashboardCtrl.js
+++ app/scripts/controllers/dashboardCtrl.js
@@ -2,10 +2,27 @@
 
 angular.module('epermitjsApp')
   .controller('DashboardCtrl', function ($scope) {
-    $scope.test = 'test'
     $scope.applications = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      {
+        appId: 1,
+        revId: 0,
+        status: null,
+        activity: 'Well',
+        proponents: "test1"
+      },
+      {
+        appId: 2,
+        revId: 0,
+        status: null,
+        activity: 'Pipeline',
+        proponents: "test2"
+      },
+      {
+        appId: 3,
+        revId: 0,
+        status: null,
+        activity: 'Well, Pipeline',
+        proponents: "test3"
+      }
     ];
   });
