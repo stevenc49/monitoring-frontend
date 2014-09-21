4ca5171d8091dc6ddb51ab00848138112d9e9f5a
Steven Cheong <stevenc49@gmail.com>
2014-09-20 23:02:18 -0700

    changed footer

--- app/scripts/controllers/dashboardCtrl.js
+++ app/scripts/controllers/dashboardCtrl.js
@@ -26,10 +26,4 @@ angular.module('epermitjsApp')
       }
     ];
 
-$scope.users = [{name: "Moroni", age: 50},
-                {name: "Tiancum", age: 43},
-                {name: "Jacob", age: 27},
-                {name: "Nephi", age: 29},
-                {name: "Enos", age: 34}];
-
   });
