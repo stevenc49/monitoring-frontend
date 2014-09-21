9fe4793a6589ffa3be64e96dba287863afc398b1
Steven Cheong <stevenc49@gmail.com>
2014-09-20 22:49:27 -0700

    added ng-table

--- app/scripts/controllers/dashboardCtrl.js
+++ app/scripts/controllers/dashboardCtrl.js
@@ -25,4 +25,11 @@ angular.module('epermitjsApp')
         proponents: "test3"
       }
     ];
+
+$scope.users = [{name: "Moroni", age: 50},
+                {name: "Tiancum", age: 43},
+                {name: "Jacob", age: 27},
+                {name: "Nephi", age: 29},
+                {name: "Enos", age: 34}];
+    
   });
