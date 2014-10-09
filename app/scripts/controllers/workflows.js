9982292ec08b63a7970bccb41a163dec43a58414
Steven Cheong <stevenc49@gmail.com>
2014-10-09 01:07:17 -0700

    socket.io responding to server

--- app/scripts/controllers/workflows.js
+++ app/scripts/controllers/workflows.js
@@ -17,207 +17,25 @@ angular.module('epermitjsApp')
 
     $scope.workflows = [
         {
-            name: 'express1',
+            name: 'express1asdfasfasfsa',
             url: 'http://localhost:3000/workflows/good.json',
             status: 'NOK',
-            lastUpdated: '2014-10-03'
+            lastUpdated: '2014-10-03',
+            duration: '1 second'
         },
         {
             name: 'express2',
             url: 'http://localhost:3000/workflows/wferror.json',
             status: 'NOK',
-            lastUpdated: '2014-10-03'
+            lastUpdated: '2014-10-03',
+            duration: '500 millisecond'
         },
         {
             name: 'express3',
             url: 'http://localhost:3000/workflows/error.json',
             status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'test_geo',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'EPermitSearch',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingAlrWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'localGovernmentJurisdictionWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'outsidePrrdNrrdIndicatorWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'fnCommunityInNebcWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'fnCommunityNoticeOnlyWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'fnCommunityOutNebcWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'forestDistrictWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'geoBcgsMapsheetWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'geoCrownLengthAndTotalAreaWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'geoPrivateLengthAndTotalAreaWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingIndianReserveWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingLandActAgrLeaseWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingLandActExcAgrLeaseWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingLandActLicenseWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingLandActPermitWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-                {
-            name: 'areaOverlappingLandActReserveWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingLandActStatRightOfWayWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingOtherFederalLandWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'landAoUntenuredCrLandWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'landCrownLandTypeWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'landTotalApplicationAreaWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'overviewOgcZoneWf',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaEstablishedByOgcWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaEstablishedByOrderWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'areaOverlappingParkWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'mkmaIntersectWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'preTenureMkmaIntersectWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'resourceManagementZoneWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
-        },
-        {
-            name: 'wellBcgsMapsheetsWf',
-            url: '#/workflows',
-            status: 'NOK',
-            lastUpdated: '2014-10-03'
+            lastUpdated: '2014-10-03',
+            duration: '14 seconds'
         }
     ];
 
